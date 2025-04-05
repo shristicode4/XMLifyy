edc277a4bec1f0343b74873ffb10731cff8ee543
Shristicode4 <shristikaadhikari88@gmail.com>
2025-04-05 14:38:47 +0530

    deploy cors error fix

--- Backend/index.js
+++ Backend/index.js
@@ -32,33 +32,25 @@ const History = mongoose.model("History", historySchema);
 // ✅ Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:10000",
-  "https://xmlifyy.onrender.com",
+  "https://xmlifyy-1.onrender.com", // ✅  frontend Render link here
 ];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-  }
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  next();
-});
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/auth", authRoutes);
 
